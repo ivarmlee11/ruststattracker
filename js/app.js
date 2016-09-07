1cfd3fde0f2b18fdace6619764ebc0efec79ddee
Ivar Lee <ivarr5@gmail.com>
2016-09-06 17:46:12 -0700

    i love angular

--- js/app.js
+++ js/app.js
@@ -1,17 +1,29 @@
 var rustats = angular.module('rustatsApp', []);
 
 rustats.controller('HomeCtrl', ['$scope', '$http', function($scope, $http) {
-  var req = {
-    url: 'http://pwnserver.apmnerdery.com:8888/getPlayersGlobalStats',
-    method: 'GET'
-  };
 
-  $scope.players = [];
+  $scope.searchTerm = '';
+  $scope.player = '';
+  $scope.playerStats = {};
 
-  $http(req).then(function success(res) {
-    $scope.players = res.data.players;
-    console.log(res.data.players);
-  }, function error(res) {
-    console.log(res);
-  });
+  $scope.search = function() {
+    req = {
+      url: 'http://pwnserver.apmnerdery.com:8888/getPlayersGlobalStats',
+      method: 'GET'
+    };
+
+    $http(req).then(function success(res) {
+      for (var i = 0; i < res.data.players.length; i++) {
+        if (res.data.players[i].playerName === $scope.searchTerm) {
+          console.log('found ' +  res.data.players[i].playerName);
+          $scope.player = res.data.players[i].playerName;
+          $scope.playerStats = res.data.players[i];
+        };
+      };
+    }, function error(res) {
+      console.log(res);
+    });
+  }
+
+
 }]);
