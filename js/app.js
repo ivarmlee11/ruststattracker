a813ac94ba8cc873590b29c73bca5912adcea7f8
Ivar Lee <ivarr5@gmail.com>
2016-09-06 21:40:01 -0700

    messing with eslint and the associated config file

--- js/app.js
+++ js/app.js
@@ -1,29 +1,33 @@
 var rustats = angular.module('rustatsApp', []);
 
-rustats.controller('HomeCtrl', ['$scope', '$http', function($scope, $http) {
-
+rustats.controller('HomeCtrl', ['$scope', '$http', function dependencies ($scope, $http) {
   $scope.searchTerm = '';
   $scope.player = '';
   $scope.playerStats = {};
+  $scope.message = '';
 
-  $scope.search = function() {
-    req = {
+  $scope.search = function search() {
+    var req = {
       url: 'http://pwnserver.apmnerdery.com:8888/getPlayersGlobalStats',
-      method: 'GET'
+      method: 'GET',
     };
 
     $http(req).then(function success(res) {
       for (var i = 0; i < res.data.players.length; i++) {
         if (res.data.players[i].playerName === $scope.searchTerm) {
-          console.log('found ' +  res.data.players[i].playerName);
+          $scope.message = '';
           $scope.player = res.data.players[i].playerName;
           $scope.playerStats = res.data.players[i];
-        };
-      };
+          break;
+        } else {
+          $scope.player = '';
+          $scope.message = 'Sorry, there is no player by that name.';
+          $scope.playerStats = {};
+          break;
+        }
+      }
     }, function error(res) {
       console.log(res);
     });
-  }
-
-
+  };
 }]);
